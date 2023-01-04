f7d867b01248f0940b7a3376643141937396254f
AngeTia <tiaange03@gmail.com>
2023-01-04 18:44:14 +0000

    Exercice 4  commit

--- EXERCICE4/script_fourth.js
+++ EXERCICE4/script_fourth.js
@@ -11,24 +11,23 @@ const allBooks = [
         "alreadyRead": false
        }
 ];
-// Récuperer
+// Récuperer la div
 const listBooks = document.querySelector('.listBooks');
 
-//
+// Créer une table
 const table = document.createElement('table');
 
-//
+// Ajouter la table à la div
 listBooks.appendChild(table);
 
-//
 allBooks.forEach(book => {
-    //
+    // Creer l'élément tr de la table
     const row = document.createElement('tr');
 
-    //
+    // Contenu des titres et auteurs
     row.innerHTML = `<td>${book.title}</td><td>${book.author}</td>`
 
-    // 
+    // Récuperation de l'image et ajout des paramètre
     const imgCell = document.createElement('td');
     const img = document.createElement('img');
     img.src = book.image;
@@ -36,9 +35,11 @@ allBooks.forEach(book => {
     imgCell.appendChild(img);
     row.appendChild(imgCell);
 
+    // Condition de verification de la lecture
     if (book.alreadyRead) {
         row.style.color = 'red';
     }
 
+    // Ajout de tous les row à la table
     table.appendChild(row);
 })
