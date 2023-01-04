878bf68cfe835a97a811bb1e7a8f6a7a7b5a7c6c
AngeTia <tiaange03@gmail.com>
2023-01-04 09:37:41 +0000

    Mise à jour de l'exercice 4

--- EXERCICE4/script_fourth.js
+++ EXERCICE4/script_fourth.js
@@ -14,6 +14,7 @@ let allBooks = [
 ];
 function createTable() {
     let maTable = document.createElement("table");
+    let maTable2 = document.createElement("table");
 
     let firstBook = Object.entries(allBooks[0]);
     let secondBook = Object.entries(allBooks[1]);
@@ -30,7 +31,9 @@ function createTable() {
         <td>${firstBook[1][1]}</td>
         <td>${firstBook[2][1]}</td>
         <td>${firstBook[3][1]}</td>
-    </tr>
+    </tr>`;
+
+    maTable2.innerHTML = `
     <tr>
         <td>${secondBook[0][0]}</td>
         <td>${secondBook[1][0]}</td>
@@ -42,11 +45,16 @@ function createTable() {
         <td>${secondBook[1][1]}</td>
         <td>${secondBook[2][1]}</td>
         <td>${secondBook[3][1]}</td>
-    </tr>
-    `;
+    </tr>`;
     maTable.setAttribute('border', "");
     maTable.style.margin = "2rem auto";
     maTable.style.textAlign= "center";
     document.body.appendChild(maTable);
+    maTable2.setAttribute('border', "");
+    maTable2.style.margin = "2rem auto";
+    maTable2.style.textAlign= "center";
+    document.body.appendChild(maTable2);
+
+   
 }
 createTable()
